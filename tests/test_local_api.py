7a5af6c0c812c17149dc679b2b257a6426a430c1
[QA3]박지우 <zu_p702@naver.com>
2026-01-09 13:23:14 +0900

    test local api 수정 커밋

--- tests/test_local_api.py
+++ tests/test_local_api.py
@@ -10,5 +10,5 @@ import requests
 
 #     assert response.status_code == 200
 
-#     # /hello 는 문자열만 내려준다고 가정
-#     assert response.text == "hello"
+    # /hello 는 문자열만 내려준다고 가정
+   # assert response.text == "hello"
